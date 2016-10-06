500f5a4feb8f600531824d1245e4bcf95bc41ed5
jorgevff <jvalladaresfontano@gmail.com>
2016-10-07 00:38:56 +0200

    Fichero anterior añadiendo edad

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,6 +2,7 @@ public class HolaMundo{
 	//el metodo principal. esto es un comentario
 	public static void main(String args[]){
 		System.out.println("Jorge Valladares xD");
+		System.out.println("45 años");
 	
 	}
 
